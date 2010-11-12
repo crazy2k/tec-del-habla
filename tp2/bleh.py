18a2d9ddcd3a378e353dd8f728b1f57f70cf8c75
Pablo Antonio <pabloa@gmail.com>
2010-11-12 05:17:48 -0300

    Agregar atributos adicionales

--- tp2/bleh.py
+++ tp2/bleh.py
@@ -7,16 +7,20 @@ INPUT_FNAME = "tp2-textos.txt"
 
 FREELING_CFG = "/usr/share/FreeLing/config/es.cfg"
 TEMP_FNAME = "TEMP"
+
 TAG_SIZE = 8
+VOWELS = ["a", "e", "i", "o", "u"]
 
 def get_POS_tags(splt_sentence):
+
+    def append_tag(l, item):
+        if "+" in item:
+            item = item.split("+")[0]
+        l.append(item)
+
     sentence = " ".join(splt_sentence)
     os.system("echo %s | analyze --nortk --outf tagged -f %s > %s" %\
         (sentence, FREELING_CFG, TEMP_FNAME))
-
-    def get_word_tag(line):
-        word, _, tag, _ = line.split()
-        return word, tag
 
     # tomamos las palabras y tags de la salida de FreeLing
     words = [line.split()[0] for line in open(TEMP_FNAME) \
@@ -30,62 +34,45 @@ def get_POS_tags(splt_sentence):
     while j < len(words) and i < len(splt_sentence):
         # si la palabra taggeada y la que queremos taggear coinciden...
         if splt_sentence[i] == words[j]:
-            ret.append(tags[j])
+            append_tag(ret, tags[j])
         # si no, puede ser que FreeLing haya juntado la palabra con otras
         elif "_" in words[j]:
             # en ese caso, a todas les ponemos el mismo tag
-            ret.append(tags[j])
+            append_tag(ret, tags[j])
             for x in range(words[j].count("_")):
-                ret.append(tags[j])
+                append_tag(ret, tags[j])
                 i += 1
             
+            # chequeamos el caso patologico en el que se separo una contraccion
+            # y se unio la primera parte a esta palabra
             if words[j].split("_")[-1].lower() in ["a", "de"] \
                 and words[j + 1] == "el":
                 j += 1
         # o puede que FreeLing haya partido la palabra en varias
         else:
-            ret.append(tags[j])
+            append_tag(ret, tags[j])
+
             # asumimos que si llegamos aca splt_sentence[i] es "al" o "del"
-            if splt_sentence[i].lower() not in ["al", "del"]:
-                sys.stdout.write("WHAT! " + str(splt_sentence) + "\n")
-                sys.stdout.write("WHAT! " + str(words) + "\n")
-                sys.stdout.write(splt_sentence[i] + " " + words[j] + "\n")
-                sys.stdout.write(str(i) + " " + str(j) + "\n")
-                return
-
             j += 1
-
-
-            while False:
-                if i + 1 < len(splt_sentence):
-                    if splt_sentence[i + 1] == "al":
-                        j = words[j:].index("a") + j
-                        ret.append(tags[j])
-                    if splt_sentence[i + 1] == "del":
-                        j = words[j:].index("de") + j
-                        ret.append(tags[j])
-                    break
-
-                j += 1
-                if j >= len(words) or i + 1 >= len(splt_sentence) \
-                    or splt_sentence[i + 1] == words[j + 1] \
-                    or "_" in words[j + 1]:
-                    break
 
         j += 1
         i += 1
 
-    print splt_sentence
-    print ret
     assert (len(splt_sentence) == len(ret))
 
     return ret
 
 def get_attributes(word_data):
-    wd = word_data
-    fmt = "%s,%s\n"
-    return ""
-    return fmt % (wd["words_before"], wd["words_after"])
+    keys = word_data.keys()
+    keys.sort()
+    # joint tiene que ir al final
+    keys.remove("joint")
+    keys.append("joint")
+
+    # reemplazamos cada clave por su valor
+    fields = [str(word_data[k]) for k in keys]
+
+    return ",".join(fields)
 
 def process_file(fname):
     ifile = open(fname)
@@ -106,7 +93,8 @@ def process_file(fname):
         splt_sentence = [wj.split(":")[0] for wj in splt_lbl_sentence]
 
         # tomamos la informacion de POS tagging
-        for i, tag in enumerate(get_POS_tags(splt_sentence)):
+        pos_tags = get_POS_tags(splt_sentence)
+        for i, tag in enumerate(pos_tags):
             # llenamos los campos que faltan con cero hasta TAG_SIZE
             tag = tag.ljust(TAG_SIZE, "0")
             # cada atributo del tag es un atributo distinto en el ARFF
@@ -114,21 +102,68 @@ def process_file(fname):
                 key = "attr_%d" % j
                 words_data[i][key] = attr
 
+            if i + 1 < len(pos_tags):
+                words_data[i]["next_is_verb"] = pos_tags[i + 1][0] == "V"
+            else:
+                words_data[i]["next_is_verb"] = False
+
         # conseguimos mas atributos
         for i, word in enumerate(splt_sentence):
-            words_data[i]["words_before"] = i
-            words_data[i]["words_after"] = len(splt_sentence) - (i + 1)
+            wd = words_data
 
-            sys.stdout.write(get_attributes(words_data[i]))
+            wd[i]["words_before"] = i
+            wd[i]["words_before_perc"] = \
+                wd[i]["words_before"]/float(len(splt_sentence))
+            wd[i]["words_after"] = len(splt_sentence) - (i + 1)
+            wd[i]["words_after_perc"] = \
+                wd[i]["words_after"]/float(len(splt_sentence))
+            wd[i]["first_word"] = i == 0
+            wd[i]["last_word"] = i == (len(splt_sentence) - 1)
+            wd[i]["ends_with"] = "V" if word[-1] in VOWELS else "C"
+            wd[i]["length"] = len(word)
+
+            if i + 1 < len(splt_sentence):    
+                wd[i]["next_starts_with"] = \
+                    "V" if splt_sentence[i + 1][0] in VOWELS else "C"
+                wd[i]["next_length"] = len(splt_sentence[i + 1])
+                wd[i]["next_word"] = splt_sentence[i + 1]
+            else:
+                wd[i]["next_starts_with"] = "N"
+                wd[i]["next_length"] = 0
+                wd[i]["next_word"] = "1r03r03032"
+
+            sys.stdout.write("%s\n" % get_attributes(words_data[i]))
  
 
 if __name__ == "__main__":
     header = """
 @RELATION joint
 
-@ATTRIBUTE wordsbefore  NUMERIC
-@ATTRIBUTE wordsafter   NUMERIC
-@ATTRIBUTE class        (0,1,2,3,4)
+@ATTRIBUTE attr_0           STRING
+@ATTRIBUTE attr_1           STRING
+@ATTRIBUTE attr_2           STRING
+@ATTRIBUTE attr_3           STRING
+@ATTRIBUTE attr_4           STRING
+@ATTRIBUTE attr_5           STRING
+@ATTRIBUTE attr_6           STRING
+@ATTRIBUTE attr_7           STRING
+@ATTRIBUTE endswith         STRING
+@ATTRIBUTE firstword        {0,1}
+@ATTRIBUTE lastword         {0,1}
+@ATTRIBUTE length           NUMERIC
+@ATTRIBUTE nextisverb       STRING
+@ATTRIBUTE nextlength       NUMERIC
+@ATTRIBUTE nextstartswith   STRING
+@ATTRIBUTE nextword         STRING
+@ATTRIBUTE word             STRING
+@ATTRIBUTE wordsafter       NUMERIC
+@ATTRIBUTE wordsafterperc   NUMERIC
+@ATTRIBUTE wordsbefore      NUMERIC
+@ATTRIBUTE wordsbeforeperc  NUMERIC
+
+@ATTRIBUTE class        {0,1,2,3,4}
+
+@DATA
 """
     sys.stdout.write(header)
 
